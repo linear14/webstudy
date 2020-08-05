6bd519fcc7b65adad1dd49b935ed134b2ae506e2
linear14 <47495592+linear14@users.noreply.github.com>
2020-08-05 18:51:44 +0900

    day3 javascript doit
    Do it 자바스크립트 기본 (13~91pg)
    1. 안녕? 자바스크립트!
    2. 자바스크립트와 친해지기
    3. 변수와 자료형 그리고 연산자

---
+++ javascript/js/js_002.js
@@ -0,0 +1,9 @@
+var paragraph = document.querySelector('#text');
+paragraph.onclick = function() {
+    paragraph.style.color = "green"
+}
+
+var heading = document.querySelector('#heading');
+heading.onclick = function() {
+    heading.style.color = "red";
+}
